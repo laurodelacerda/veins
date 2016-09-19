a99bafa237d9284b5c361e2d81c265d26da41989
Lauro de Lacerda <laurodelacerda@gmail.com>
2016-09-19 18:15:25 -0300

    Added functions to: make vehicles change lane and set lane change mode

--- src/veins/modules/mobility/traci/TraCICommandInterface.cc
+++ src/veins/modules/mobility/traci/TraCICommandInterface.cc
@@ -590,4 +590,21 @@ std::list<Coord> TraCICommandInterface::genericGetCoordList(uint8_t commandId, s
 	return res;
 }
 
+void TraCICommandInterface::Vehicle::setLaneChangeMode(int32_t bitset){
+    uint8_t variableId = VAR_LANECHANGE_MODE;
+    uint8_t variableType = TYPE_INTEGER;
+    TraCIBuffer buf = traci->connection.query(CMD_SET_VEHICLE_VARIABLE, TraCIBuffer() << variableId << nodeId << variableType << bitset);
+    ASSERT(buf.eof());
+}
+
+void TraCICommandInterface::Vehicle::changeLane(uint8_t laneIndex, int32_t time){
+    uint8_t variableId = CMD_CHANGELANE;
+    uint8_t variableType = TYPE_COMPOUND;
+    int32_t count = 2;
+    uint8_t laneIndexType = TYPE_BYTE;
+    uint8_t durationType = TYPE_INTEGER;
+    TraCIBuffer buf = connection->query(CMD_SET_VEHICLE_VARIABLE, TraCIBuffer() << variableId << nodeId << variableType << count << laneIndexType << laneIndex << durationType << time);
+        ASSERT(buf.eof());
+}
+
 }
--- src/veins/modules/mobility/traci/TraCICommandInterface.h
+++ src/veins/modules/mobility/traci/TraCICommandInterface.h
@@ -55,6 +55,9 @@ class TraCICommandInterface
 				std::string getTypeId();
 				bool changeVehicleRoute(const std::list<std::string>& roads);
 
+                void changeLane(uint8_t laneIndex, int32_t time);
+                void setLaneChangeMode (int32_t bitset);
+
 			protected:
 				TraCICommandInterface* traci;
 				TraCIConnection* connection;
